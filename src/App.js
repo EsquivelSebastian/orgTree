02d6c83e350f4c29fd6ae46f1597b0c959d89481
Sebastian A. Esquivel <71994621+EsquivelSebastian@users.noreply.github.com>
2022-10-17 22:59:00 -0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+        import OrgChart from './mytree';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+        export default class App extends Component {
+            render() {
+                return (
+                    <div style={{height: '100%'}}>
 
-export default App;
+                        <OrgChart nodes={[
+                            { id: 1, name: 'Denny Curtis', title: 'CEO', img: 'https://cdn.balkan.app/shared/2.jpg' },
+                            { id: 2, pid: 3, name: 'Ashley Barnett', title: 'Sales Manager', img: 'https://cdn.balkan.app/shared/3.jpg' },
+                            { id: 3, pid: 1, name: 'Caden Ellison', title: 'Dev Manager', img: 'https://cdn.balkan.app/shared/4.jpg' },
+                            { id: 4, pid: 3, name: 'Elliot Patel', title: 'Sales', img: 'https://cdn.balkan.app/shared/5.jpg' },
+                            { id: 5, pid: 1, name: 'Lynn Hussain', title: 'Sales', img: 'https://cdn.balkan.app/shared/6.jpg' },
+                            { id: 6, pid: 4, name: 'Tanner May', title: 'Developer', img: 'https://cdn.balkan.app/shared/7.jpg' },
+                            { id: 7, pid: 6, name: 'Fran Parsons', title: 'Developer', img: 'https://cdn.balkan.app/shared/8.jpg' },
+                            { id: 8, pid: 1, name: 'Sebastian Esquivel', title: 'Web Developer', img: 'https://media-exp1.licdn.com/dms/image/C4E03AQHXbFMwUIcepQ/profile-displayphoto-shrink_100_100/0/1516697256126?e=1671667200&v=beta&t=rDdxrdRmGRr-h-H51HYmdk6spUTaRYaEx9Vpiq6TNqI' }
+
+                        ]} />
+                    </div>
